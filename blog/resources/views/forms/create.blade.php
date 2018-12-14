6617634ef61aee13980cd83327133aa3055b1079
Jorrit Landman <97041160@st.deltion.nl>
2018-12-14 12:49:31 +0100

    alle bestanden overgezet naar git

---
+++ blog/resources/views/forms/create.blade.php
@@ -0,0 +1,95 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Form Validation using jQuery in Laravel</title>
+    <link rel="stylesheet" href="{{asset('css/app.css')}}">
+    <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet">  
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.js"></script>  
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"></script>  
+  </head>
+  <body>
+    <div class="container">
+      <h2>Form Validation using jQuery</h2><br/>
+      <form method="post" action="{{url('forms')}}" id="form">
+        @csrf
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <label for="Name">Name:</label>
+            <input type="text" class="form-control" name="name">
+          </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Email">Email:</label>
+              <input type="text" class="form-control" name="email">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Number">Phone Number:</label>
+              <input type="text" class="form-control" name="number">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Min Length">Min Length(minium 5):</label>
+              <input type="text" class="form-control" name="minlength">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Max Length">Max Length(maximum 8):</label>
+              <input type="text" class="form-control" name="maxlength">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Min Value">Min Value(minium 1):</label>
+              <input type="text" class="form-control" name="minvalue">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Max Value">Max Value(maximum value 100):</label>
+              <input type="text" class="form-control" name="maxvalue">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Range">Range(minium 20, maximum 40):</label>
+              <input type="text" class="form-control" name="range">
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-4"></div>
+            <div class="form-group col-md-4">
+              <label for="Range">URL:</label>
+              <input type="text" class="form-control" name="url">
+            </div>
+          </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4">
+            <input type="file" name="filename">    
+         </div>
+        </div>
+        <div class="row">
+          <div class="col-md-4"></div>
+          <div class="form-group col-md-4" style="margin-top:60px">
+            <button type="submit" class="btn btn-success">Submit</button>
+          </div>
+        </div>
+      </form>
+    </div>
+  </body>
+
+</html>
